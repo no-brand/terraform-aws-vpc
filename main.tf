eddb93085f7186fee8ba92fdb8010728748e9c07
no-brand <do.dream.david@gmail.com>
2022-04-18 09:42:53 +0900

    resource: public subnet route table

--- main.tf
+++ main.tf
@@ -32,3 +32,25 @@ resource "aws_subnet" "public" {
     "Name" = format("%s-public-subnet", local.prefix_hyphen)
   }, var.tags)
 }
+
+# {namespace}-{stage}-{region}-public-subnet-rtb
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = merge({
+    "Name" = format("%s-public-subnet-rtb", local.prefix_hyphen)
+  }, var.tags)
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = {for az_id, subnet in var.public_subnets: "${var.region}${az_id}" => subnet}
+
+  subnet_id      = aws_subnet.public[each.key].id
+  route_table_id = aws_route_table.public.id
+}
